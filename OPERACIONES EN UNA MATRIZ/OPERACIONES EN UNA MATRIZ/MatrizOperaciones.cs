cace1279533451f68f9a0162fc50ac527d4074c7
Alejandro Toro <diegoguerrero2789@gmail.com>
2025-09-17 21:52:26 -0500

    The file program and the MatrizOperaciones class are created

---
+++ OPERACIONES EN UNA MATRIZ/OPERACIONES EN UNA MATRIZ/MatrizOperaciones.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Taller01
+{
+    public class MatrizOperaciones
+    {
+        private int[,] matriz;
+        private int n;
+
+        public MatrizOperaciones(int n)
+        {
+            this.n = n;
+            matriz = new int[n, n];
+            LlenarMatriz();
+        }
+
+        private void LlenarMatriz()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matriz[i, j] = (i + 1) - j;
+                }
+            }
+        }
+
+        public void MostrarMatriz()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(matriz[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public int Sumatoria()
+        {
+            int suma = 0;
+            foreach (int valor in matriz)
+            {
+                suma += valor;
+            }
+            return suma;
+        }
+        public int Maximo()
+        {
+            int max = matriz[0, 0];
+            foreach (int valor in matriz)
+            {
+                if (valor > max) max = valor;
+            }
+            return max;
+        }
+
+        public int Minimo()
+        {
+            int min = matriz[0, 0];
+            foreach (int valor in matriz)
+            {
+                if (valor < min) min = valor;
+            }
+            return min;
+        }
+    }
+}
---
+++ OPERACIONES EN UNA MATRIZ/OPERACIONES EN UNA MATRIZ/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Taller01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese orden de la matriz: ");
+            int n = int.Parse(Console.ReadLine());
+
+            MatrizOperaciones matrizOp = new MatrizOperaciones(n);
+
+            matrizOp.MostrarMatriz();
+
+            Console.WriteLine($"La sumatoria es: {matrizOp.Sumatoria()}");
+            Console.WriteLine($"El valor máximo es: {matrizOp.Maximo()}");
+            Console.WriteLine($"El valor mínimo es: {matrizOp.Minimo()}");
+        }
+    }
+}
