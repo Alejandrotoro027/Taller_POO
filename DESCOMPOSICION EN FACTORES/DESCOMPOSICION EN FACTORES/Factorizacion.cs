1d0f23781f858a89a80ba99fe9e6917110bcd40e
Alejandro Toro <diegoguerrero2789@gmail.com>
2025-09-17 22:07:46 -0500

    We created the program and then the Factorizacion class.

---
+++ DESCOMPOSICION EN FACTORES/DESCOMPOSICION EN FACTORES/Factorizacion.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Taller01
+{
+    public class Factorizacion
+    {
+        private int numero;
+
+        public Factorizacion(int numero)
+        {
+            if (numero <= 1)
+                throw new ArgumentException("El número debe ser mayor que 1.");
+
+            this.numero = numero;
+        }
+
+        public List<int> ObtenerFactores()
+        {
+            List<int> factores = new List<int>();
+            int n = numero;
+
+            while (n % 2 == 0)
+            {
+                factores.Add(2);
+                n /= 2;
+            }
+
+            for (int i = 3; i <= Math.Sqrt(n); i += 2)
+            {
+                while (n % i == 0)
+                {
+                    factores.Add(i);
+                    n /= i;
+                }
+            }
+
+            if (n > 1)
+            {
+                factores.Add(n);
+            }
+
+            return factores;
+        }
+        public string MostrarFactores()
+        {
+            var factores = ObtenerFactores();
+            return $"{numero} = {string.Join(" × ", factores)}";
+        }
+    }
+}
---
+++ DESCOMPOSICION EN FACTORES/DESCOMPOSICION EN FACTORES/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Taller01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el número a descomponer: ");
+            int num = int.Parse(Console.ReadLine());
+
+            try
+            {
+                Factorizacion fact = new Factorizacion(num);
+                Console.WriteLine(fact.MostrarFactores());
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+    }
+}
